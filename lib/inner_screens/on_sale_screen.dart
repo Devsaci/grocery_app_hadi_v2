4bb4df2da2d612bfa2c830df0658f0335010c279
saci zakaria <sacideveloppeur@gmail.com>
2023-01-13 10:41:56 +0100

    Image.asset  box.png

--- lib/inner_screens/on_sale_screen.dart
+++ lib/inner_screens/on_sale_screen.dart
@@ -50,6 +50,12 @@ class _OnSaleScreenState extends State<OnSaleScreen> {
                 padding: const EdgeInsets.all(8.0),
                 child: Column(
                   children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.asset(
+                        'assets/images/box.png',
+                      ),
+                    ),
                     Text(
                       'No products on sale yet!,\nStay tuned',
                       textAlign: TextAlign.center,
