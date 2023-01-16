f4900e5d53c0a16b2bf56d02c62134c23779c791
saci zakaria <sacideveloppeur@gmail.com>
2023-01-16 14:20:58 +0100

    33. Implement the feeds screen

---
+++ lib/inner_screens/feeds_screen.dart
@@ -0,0 +1,12 @@
+// 33. Implement the feeds screen
+
+import 'package:flutter/material.dart';
+
+class FeedsScreen extends StatelessWidget {
+  const FeedsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
