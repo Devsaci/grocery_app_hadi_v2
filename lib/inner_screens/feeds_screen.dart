814708917ecfa1cb6e1012f1003ae583687e0e21
saci zakaria <sacideveloppeur@gmail.com>
2023-01-16 14:24:41 +0100

    AppBar

--- lib/inner_screens/feeds_screen.dart
+++ lib/inner_screens/feeds_screen.dart
@@ -1,6 +1,9 @@
 // 33. Implement the feeds screen
 
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:grocery_app_hadi_v1/services/utils.dart';
+import 'package:grocery_app_hadi_v1/widgets/text_widget.dart';
 
 class FeedsScreen extends StatefulWidget {
   const FeedsScreen({super.key});
@@ -12,6 +15,30 @@ class FeedsScreen extends StatefulWidget {
 class _FeedsScreenState extends State<FeedsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Color color = Utils(context).color;
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: InkWell(
+          borderRadius: BorderRadius.circular(12),
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Icon(
+            IconlyLight.arrowLeft2,
+            color: color,
+          ),
+        ),
+        elevation: 0,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        centerTitle: true,
+        title: TextWidget(
+          text: 'All Products',
+          color: color,
+          textSize: 20.0,
+          isTitle: true,
+        ),
+      ),
+    );
   }
 }
