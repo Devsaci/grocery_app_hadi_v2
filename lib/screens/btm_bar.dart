1805ba393ee59f5a535ab37ebe9159c3b6f8cf82
saci zakaria <sacideveloppeur@gmail.com>
2023-01-24 10:33:04 +0100

    int _selectedIndex = 2;

--- lib/screens/btm_bar.dart
+++ lib/screens/btm_bar.dart
@@ -17,7 +17,7 @@ class BottomBarScreen extends StatefulWidget {
 }
 
 class _BottomBarScreenState extends State<BottomBarScreen> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 2;
   final List<Map<String, dynamic>> _pages = [
     {
       'page': const HomeScreen(),
