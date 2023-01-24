d88c799433f961f63784518ad616029b5d7c0246
saci zakaria <sacideveloppeur@gmail.com>
2023-01-24 10:46:11 +0100

    CartWidget

---
+++ lib/screens/cart/cart_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CartWidget extends StatefulWidget {
+  const CartWidget({super.key});
+
+  @override
+  State<CartWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<CartWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
