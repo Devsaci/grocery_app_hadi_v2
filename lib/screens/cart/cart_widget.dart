f8ff282293a364561764ab0a13f327a7dc607bb3
saci zakaria <sacideveloppeur@gmail.com>
2023-01-30 12:15:13 +0100

    inputFormatters: [],

--- lib/screens/cart/cart_widget.dart
+++ lib/screens/cart/cart_widget.dart
@@ -79,6 +79,7 @@ class _CartWidgetState extends State<CartWidget> {
                             borderSide: BorderSide(),
                           ),
                         ),
+                        inputFormatters: [],
                       ),
                     ),
                     _quantityController(
