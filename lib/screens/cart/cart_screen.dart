bdbd8305de4dcc406b473f778658d00aa08315af
saci zakaria <sacideveloppeur@gmail.com>
2023-01-31 11:36:43 +0100

    35. Implement the cart screen

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
 
 import 'cart_widget.dart';
 
@@ -8,14 +9,22 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(IconlyBold.delete),
+            ),
+          ],
+        ),
         body: ListView.builder(
-      itemCount: 10,
-      itemBuilder: (ctx, index) {
-        return const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: CartWidget(),
-        );
-      },
-    ));
+          itemCount: 10,
+          itemBuilder: (ctx, index) {
+            return const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: CartWidget(),
+            );
+          },
+        ));
   }
 }
