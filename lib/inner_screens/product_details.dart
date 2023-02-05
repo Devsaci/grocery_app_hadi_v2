ae726029f91a1162da95585c90b51b2229618a95
saci zakaria <sacideveloppeur@gmail.com>
2023-02-05 19:13:30 +0100

    Text('ProductDetails')

--- lib/inner_screens/product_details.dart
+++ lib/inner_screens/product_details.dart
@@ -11,6 +11,8 @@ class ProductDetails extends StatefulWidget {
 class _ProductDetailsState extends State<ProductDetails> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Center(
+      child: Text('ProductDetails'),
+    );
   }
 }
