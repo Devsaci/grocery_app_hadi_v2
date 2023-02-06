e3b618d40acb3bf52a33703a3ba6b4aee7aa489d
saci zakaria <sacideveloppeur@gmail.com>
2023-02-06 16:25:36 +0100

    InkWell

--- lib/inner_screens/product_details.dart
+++ lib/inner_screens/product_details.dart
@@ -12,7 +12,9 @@ class _ProductDetailsState extends State<ProductDetails> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: InkWell(),
+      ),
       body: Center(
         child: Text('ProductDetails'),
       ),
