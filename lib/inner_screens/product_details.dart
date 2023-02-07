e53861d15bfa1b914687c84806db7523413beb8e
saci zakaria <sacideveloppeur@gmail.com>
2023-02-07 12:08:41 +0100

    Material  CupertinoIcons.minus

--- lib/inner_screens/product_details.dart
+++ lib/inner_screens/product_details.dart
@@ -1,4 +1,5 @@
 import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:grocery_app_hadi_v1/widgets/text_widget.dart';
@@ -125,6 +126,29 @@ class _ProductDetailsState extends State<ProductDetails> {
                       ],
                     ),
                   ),
+
+                  const SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Material(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.red,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: InkWell(
+                            //borderRadius: BorderRadius.circular(12),
+                            onTap: () {},
+                            child: const Icon(
+                              CupertinoIcons.minus,
+                              color: Colors.white,
+                              size: 45,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
                 ],
               ),
             ),
