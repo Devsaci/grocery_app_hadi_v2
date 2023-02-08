9761050ca865e178061701a820a38dfb97094957
saci zakaria <sacideveloppeur@gmail.com>
2023-02-08 09:23:31 +0100

    Container(),

--- lib/inner_screens/product_details.dart
+++ lib/inner_screens/product_details.dart
@@ -173,7 +173,8 @@ class _ProductDetailsState extends State<ProductDetails> {
                         ),
                       ],
                     ),
-                  )
+                  ),
+                  Container(),
                 ],
               ),
             ),
