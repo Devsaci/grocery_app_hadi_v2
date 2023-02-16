002373cc46ae5eacf4403cb1612a8092b64f643a
saci zakaria <sacideveloppeur@gmail.com>
2023-02-16 21:45:05 +0100

    home: const WishlistScreen()

--- lib/screens/wishlist/wishlist_widget.dart
+++ lib/screens/wishlist/wishlist_widget.dart
@@ -5,6 +5,8 @@ class WishlistScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
