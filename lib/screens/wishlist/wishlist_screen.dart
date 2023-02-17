04a9bcf9b1a66186824ac9a4909ffeba8b3ddd5d
saci zakaria <sacideveloppeur@gmail.com>
2023-02-17 11:20:24 +0100

    Scaffold  / appBar

--- lib/screens/wishlist/wishlist_screen.dart
+++ lib/screens/wishlist/wishlist_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:grocery_app_hadi_v1/widgets/back_widget.dart';
 
 class WishlistScreen extends StatelessWidget {
   const WishlistScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: const BackWidget(),
+      ),
+    );
   }
 }
