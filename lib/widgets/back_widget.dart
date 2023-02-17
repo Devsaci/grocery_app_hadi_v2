7a290446fac8ac7420c7335107e52ed49173b7ee
saci zakaria <sacideveloppeur@gmail.com>
2023-02-17 10:54:20 +0100

    return InkWell

--- lib/widgets/back_widget.dart
+++ lib/widgets/back_widget.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+
+import '../services/utils.dart';
 
 class BackWidget extends StatelessWidget {
   const BackWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final Color color = Utils(context).color;
+    Size size = Utils(context).getScreenSize;
+    return InkWell(
+      borderRadius: BorderRadius.circular(12),
+      onTap: () {
+        Navigator.pop(context);
+      },
+      child: Icon(
+        IconlyLight.arrowLeft2,
+        color: color,
+      ),
+    );
   }
 }
