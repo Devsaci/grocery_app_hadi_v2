e6ca36b9782812976310f4ad12fa8e5a7a156eb1
saci zakaria <sacideveloppeur@gmail.com>
2023-03-06 19:35:30 +0100

    width

--- lib/screens/orders/orders_widget.dart
+++ lib/screens/orders/orders_widget.dart
@@ -1,6 +1,8 @@
 import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
 
+import '../../services/utils.dart';
+
 class OrderWidget extends StatefulWidget {
   const OrderWidget({super.key});
 
@@ -11,10 +13,12 @@ class OrderWidget extends StatefulWidget {
 class _OrderWidgetState extends State<OrderWidget> {
   @override
   Widget build(BuildContext context) {
+    Size size = Utils(context).getScreenSize;
     return ListTile(
       subtitle: const Text('Paid: \$12.8'),
       onTap: () {},
       leading: FancyShimmerImage(
+        width: size.width * 0.2,
         imageUrl: 'https://i.ibb.co/F0s3FHQ/Apricots.png',
       ),
     );
