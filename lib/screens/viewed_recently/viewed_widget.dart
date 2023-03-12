32df0c5893d023174b1fdb4c3bc827b17069761c
saci zakaria <sacideveloppeur@gmail.com>
2023-03-12 19:24:58 +0100

    class ViewedRecentlyWidget

---
+++ lib/screens/viewed_recently/viewed_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ViewedRecentlyWidget extends StatelessWidget {
+  const ViewedRecentlyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
