3abd70a696f556e2fcefd83c522d6ed0b82c234d
saci zakaria <sacideveloppeur@gmail.com>
2023-03-12 19:25:34 +0100

    ViewedRecentlyWidget extends StatefulWidget

--- lib/screens/viewed_recently/viewed_widget.dart
+++ lib/screens/viewed_recently/viewed_widget.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class ViewedRecentlyWidget extends StatelessWidget {
+class ViewedRecentlyWidget extends StatefulWidget {
   const ViewedRecentlyWidget({super.key});
 
+  @override
+  State<ViewedRecentlyWidget> createState() => _ViewedRecentlyWidgetState();
+}
+
+class _ViewedRecentlyWidgetState extends State<ViewedRecentlyWidget> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
