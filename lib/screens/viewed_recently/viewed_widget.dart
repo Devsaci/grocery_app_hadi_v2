0f16b8b8dee8e1816cfe570f378b854a621ca3ac
saci zakaria <sacideveloppeur@gmail.com>
2023-03-15 11:56:27 +0100

    padding  GestureDetector

--- lib/screens/viewed_recently/viewed_widget.dart
+++ lib/screens/viewed_recently/viewed_widget.dart
@@ -18,59 +18,62 @@ class _ViewedRecentlyWidgetState extends State<ViewedRecentlyWidget> {
   Widget build(BuildContext context) {
     Color color = Utils(context).color;
     Size size = Utils(context).getScreenSize;
-    return GestureDetector(
-      onTap: () {
-        GlobalMethods.navigateTo(
-          ctx: context,
-          routeName: ProductDetails.routeName,
-        );
-      },
-      child: Row(
-        children: [
-          FancyShimmerImage(
-            imageUrl: 'https://i.ibb.co/F0s3FHQ/Apricots.png',
-            boxFit: BoxFit.fill,
-            height: size.width * 0.27,
-            width: size.width * 0.25,
-          ),
-          const SizedBox(width: 12),
-          Column(
-            children: [
-              TextWidget(
-                text: "Title",
-                color: color,
-                textSize: 20,
-                isTitle: true,
-              ),
-              const SizedBox(width: 12),
-              TextWidget(
-                text: '\$12.88',
-                color: color,
-                textSize: 16,
-                isTitle: false,
-              ),
-            ],
-          ),
-          const Spacer(),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 5),
-            child: Material(
-              borderRadius: BorderRadius.circular(20),
-              color: Colors.green,
-              child: InkWell(
-                onTap: () {},
-                child: const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Icon(
-                    CupertinoIcons.add,
-                    color: Colors.white,
-                    size: 30,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 18),
+      child: GestureDetector(
+        onTap: () {
+          GlobalMethods.navigateTo(
+            ctx: context,
+            routeName: ProductDetails.routeName,
+          );
+        },
+        child: Row(
+          children: [
+            FancyShimmerImage(
+              imageUrl: 'https://i.ibb.co/F0s3FHQ/Apricots.png',
+              boxFit: BoxFit.fill,
+              height: size.width * 0.27,
+              width: size.width * 0.25,
+            ),
+            const SizedBox(width: 12),
+            Column(
+              children: [
+                TextWidget(
+                  text: "Title",
+                  color: color,
+                  textSize: 20,
+                  isTitle: true,
+                ),
+                const SizedBox(width: 12),
+                TextWidget(
+                  text: '\$12.88',
+                  color: color,
+                  textSize: 16,
+                  isTitle: false,
+                ),
+              ],
+            ),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: Material(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.green,
+                child: InkWell(
+                  onTap: () {},
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(
+                      CupertinoIcons.add,
+                      color: Colors.white,
+                      size: 30,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
