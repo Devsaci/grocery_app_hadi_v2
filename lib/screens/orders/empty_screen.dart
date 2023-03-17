1427579e8b44f951433edd209f2aa1fb372a6e12
saci zakaria <sacideveloppeur@gmail.com>
2023-03-17 10:33:30 +0100

    backgroundColor

--- lib/screens/orders/empty_screen.dart
+++ lib/screens/orders/empty_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app_hadi_v1/inner_screens/feeds_screen.dart';
+import 'package:grocery_app_hadi_v1/services/global_methods.dart';
 
 import '../../services/utils.dart';
 import '../../widgets/text_widget.dart';
@@ -12,38 +14,48 @@ class EmptyScreen extends StatelessWidget {
     final Color color = Utils(context).color;
     final themeState = Utils(context).getTheme;
     return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(height: 50),
-          Image.asset("assets/images/cart.png"),
-          const SizedBox(height: 10),
-          const Text(
-            'Whoops!',
-            style: TextStyle(
-                color: Colors.red, fontSize: 40, fontWeight: FontWeight.w700),
-          ),
-          const SizedBox(height: 20),
-          TextWidget(text: "title", color: Colors.cyan, textSize: 20),
-          const SizedBox(height: 20),
-          TextWidget(text: "subtitle", color: Colors.cyan, textSize: 20),
-          SizedBox(height: size.height * 0.1),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              elevation: 5,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-                side: const BorderSide(color: Colors.black, width: 3),
+      backgroundColor: Colors.amber,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 50),
+            Image.asset("assets/images/cart.png"),
+            const SizedBox(height: 10),
+            const Text(
+              'Whoops!',
+              style: TextStyle(
+                  color: Colors.red, fontSize: 40, fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(height: 20),
+            TextWidget(text: "title", color: Colors.cyan, textSize: 20),
+            const SizedBox(height: 20),
+            TextWidget(text: "subtitle", color: Colors.cyan, textSize: 20),
+            SizedBox(height: size.height * 0.1),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  side: const BorderSide(color: Colors.black, width: 3),
+                ),
+                backgroundColor: Theme.of(context).colorScheme.secondary,
               ),
-            ),
-            onPressed: () {},
-            child: TextWidget(
-              text: "buttonText",
-              color: themeState ? Colors.grey.shade300 : Colors.grey.shade900,
-              textSize: 20,
-              isTitle: true,
-            ),
-          )
-        ],
+              onPressed: () {
+                GlobalMethods.navigateTo(
+                    ctx: context, routeName: FeedsScreen.routeName);
+              },
+              child: TextWidget(
+                text: "buttonText",
+                color: themeState ? Colors.grey.shade300 : Colors.grey.shade900,
+                textSize: 20,
+                isTitle: true,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
