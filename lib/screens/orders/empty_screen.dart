fb774c96e269e8710d800581ebc07f94626f37b1
saci zakaria <sacideveloppeur@gmail.com>
2023-03-17 09:17:18 +0100

    Scaffold

--- lib/screens/orders/empty_screen.dart
+++ lib/screens/orders/empty_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class EmptyScreen extends StatelessWidget {
   const EmptyScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
