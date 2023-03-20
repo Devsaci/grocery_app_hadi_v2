7ac10a2d18c0bfad352bb5a22a6a68d366105d86
saci zakaria <sacideveloppeur@gmail.com>
2023-03-20 18:24:18 +0100

    _isEmpty ? const EmptyScreen

--- lib/screens/wishlist/wishlist_screen.dart
+++ lib/screens/wishlist/wishlist_screen.dart
@@ -6,6 +6,7 @@ import 'package:grocery_app_hadi_v1/widgets/back_widget.dart';
 
 import '../../services/global_methods.dart';
 import '../../services/utils.dart';
+import '../../widgets/empty_screen.dart';
 import '../../widgets/text_widget.dart';
 
 class WishlistScreen extends StatelessWidget {
@@ -16,41 +17,51 @@ class WishlistScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final Color color = Utils(context).color;
     Size size = Utils(context).getScreenSize;
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        leading: const BackWidget(),
-        automaticallyImplyLeading: false,
-        elevation: 0,
-        backgroundColor: Theme.of(context).cardColor.withOpacity(0.5),
-        title: TextWidget(
-          text: 'Wishlist (2)',
-          color: color,
-          isTitle: true,
-          textSize: 22,
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              GlobalMethods.warningDialog(
-                title: 'Empty your Wishlist?',
-                subtitle: 'Are you sure?',
-                fct: () {},
-                context: context,
-              );
-            },
-            icon: Icon(
-              IconlyBroken.delete,
-              color: color,
+    bool _isEmpty = true;
+
+    return _isEmpty
+        ? const EmptyScreen(
+            title: 'Your Wishlist Is Empty',
+            subtitle: 'Explore more and shortlist some items',
+            imagePath: 'assets/images/wishlist.png',
+            buttonText: 'Add a wish',
+          )
+        // ignore: dead_code
+        : Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              leading: const BackWidget(),
+              automaticallyImplyLeading: false,
+              elevation: 0,
+              backgroundColor: Theme.of(context).cardColor.withOpacity(0.5),
+              title: TextWidget(
+                text: 'Wishlist (2)',
+                color: color,
+                isTitle: true,
+                textSize: 22,
+              ),
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    GlobalMethods.warningDialog(
+                      title: 'Empty your Wishlist?',
+                      subtitle: 'Are you sure?',
+                      fct: () {},
+                      context: context,
+                    );
+                  },
+                  icon: Icon(
+                    IconlyBroken.delete,
+                    color: color,
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
-      body: MasonryGridView.count(
-          crossAxisCount: 2,
-          itemBuilder: (context, index) {
-            return const WishlistWidget();
-          }),
-    );
+            body: MasonryGridView.count(
+                crossAxisCount: 2,
+                itemBuilder: (context, index) {
+                  return const WishlistWidget();
+                }),
+          );
   }
 }
