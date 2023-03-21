faf2ca2527f1314f980dbdd85b2357be9883176a
saci zakaria <sacideveloppeur@gmail.com>
2023-03-21 10:49:14 +0100

    Scaffold   body: Column(),

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -10,6 +10,9 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: Column(),
+    );
   }
 }
