774a4b325cf563b4eab8a2a72cb3ac27cdf94b89
saci zakaria <sacideveloppeur@gmail.com>
2023-03-23 11:41:51 +0100

    final Function fct;   final String buttonText;   final Color primary;

--- lib/widgets/auth_button.dart
+++ lib/widgets/auth_button.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
 class AuthButton extends StatelessWidget {
-  const AuthButton({super.key});
+  const AuthButton({
+    super.key,
+    required this.fct,
+    required this.buttonText,
+    required this.primary,
+  });
+  final Function fct;
+  final String buttonText;
+  final Color primary;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox();
   }
 }
