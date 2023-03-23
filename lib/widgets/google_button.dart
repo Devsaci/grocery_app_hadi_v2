2a4750408e272fe3ca00a28955c52d214300a0a5
saci zakaria <sacideveloppeur@gmail.com>
2023-03-23 18:20:45 +0100

    Material  InkWell

--- lib/widgets/google_button.dart
+++ lib/widgets/google_button.dart
@@ -5,6 +5,10 @@ class GoogleButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Material(
+      child: InkWell(
+        onTap: () {},
+      ),
+    );
   }
 }
