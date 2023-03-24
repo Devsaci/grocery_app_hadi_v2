01af1b11edc40db9c33aeb6e3a0ff2c5b5942ea9
saci zakaria <sacideveloppeur@gmail.com>
2023-03-24 09:43:04 +0100

    static const routeName = '/RegisterScreen';

--- lib/screens/auth/register.dart
+++ lib/screens/auth/register.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class RegisterScreen extends StatelessWidget {
+  static const routeName = '/RegisterScreen';
   const RegisterScreen({super.key});
 
   @override
